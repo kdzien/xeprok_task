acd33f75ac681252528bc9fa3fd5f13cf3f0c15f
Konrad <dzieniu@gmail.com>
2018-09-06 19:41:45 +0200

    basic layout

--- src/app/add-holiday/add-holiday.component.ts
+++ src/app/add-holiday/add-holiday.component.ts
@@ -8,12 +8,14 @@ import { HolidaysServiceService } from '../services/holidays-service.service';
   styleUrls: ['./add-holiday.component.css']
 })
 export class AddHolidayComponent implements OnInit {
+  private current_date = new Date().toISOString().split('T')[0];
+  private add_holiday_info = '';
   private holiday_form = {
     name: 'Konrad',
     surename: 'Dzień',
     team: 'IT',
-    from: undefined,
-    to: undefined,
+    from: this.current_date,
+    to: this.current_date,
   }
   constructor(private holidayService: HolidaysServiceService) { }
 
@@ -22,6 +24,10 @@ export class AddHolidayComponent implements OnInit {
   addHoliday(): void {
     const {name, surename, team, from, to} = this.holiday_form;
     const new_holiday = new Holiday(name, surename, team, new Date(from), new Date(to));
-    this.holidayService.addHoliday(new_holiday);
+    this.holidayService.addHoliday(new_holiday).then(response => {
+      this.add_holiday_info = response;
+    }).catch(warning => {
+      this.add_holiday_info = warning;
+    });
   }
 }
--- src/app/services/holidays-service.service.ts
+++ src/app/services/holidays-service.service.ts
@@ -14,19 +14,28 @@ export class HolidaysServiceService {
       resolve(this.holidays);
     });
   }
-  addHoliday(holiday: Holiday): void {
-    this.checkCorrectivity(holiday, this.holidays);
-    this.holidays.push(holiday);
+  addHoliday(holiday: Holiday): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (holiday.to < holiday.from) {
+        reject('Data do jest mniejsza niż data od');
+      }
+      if (this.checkCorrectivity(holiday, this.holidays)) {
+        this.holidays.push(holiday);
+        resolve('Dodałes urlop');
+      } else {
+        reject('Nie możesz wziąć urlopu w tym terminie');
+      }
+    });
   }
   private checkCorrectivity(new_holiday, holidays) {
     const date_available = holidays.filter(holiday => new_holiday.team === holiday.team)
     .filter(holiday => new_holiday.from.isBetween(holiday.from, holiday.to) || new_holiday.to.isBetween(holiday.from, holiday.to));
-    console.log(date_available);
+    return date_available.length === 0 ? true : false;
   }
 }
 Date.prototype.isBetween = function(from, to) {
   console.log(this);
-  if(this > from && this < to) {
+  if(this >= from && this <= to) {
     return true;
   }
   return false;
